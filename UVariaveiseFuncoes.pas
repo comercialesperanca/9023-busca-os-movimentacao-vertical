82bfb5e31e10e6623687d415169f968e3a5cf93e
Jhonny Oliveira <oliveira.programador@gmail.com>
2023-10-06 10:23:40 -0300

    removendo o join usando o campo NUMCAR entre as tabelas PCMOVENDPEND e BODEFINEONDAI

--- UVariaveiseFuncoes.pas
+++ UVariaveiseFuncoes.pas
@@ -115,7 +115,7 @@ begin
     begin
 
       SQL.Add(' join bodefineondai ON bodefineondai.numtranswms = pcmovendpend.numtranswms ');
-      SQL.Add('     AND bodefineondai.numcar = pcmovendpend.numcar ');
+//      SQL.Add('     AND bodefineondai.numcar = pcmovendpend.numcar ');
     end;
 
     SQL.Add(' WHERE    pcmovendpend.posicao = ''P''                                          ');
@@ -806,7 +806,7 @@ begin
     SQL.Add(' join pcendereco      on pcendereco.codendereco = pcmovendpend.codenderecoorig               ');
     SQL.Add(' left join bopendenciaconf on bopendenciaconf.numos = pcmovendpend.numos                     ');
     SQL.Add(' left join bodefineondai   on bodefineondai.numtranswms = pcmovendpend.numtranswms           ');
-    SQL.Add('     AND bodefineondai.numcar = pcmovendpend.numcar ');
+//    SQL.Add('     AND bodefineondai.numcar = pcmovendpend.numcar ');
     SQL.Add(' left join booscompendencia on booscompendencia.numos = pcmovendpend.numos                   ');
     SQL.Add('     and booscompendencia.dataliberacao is null                                              ');
 
@@ -988,7 +988,7 @@ begin
     SQL.Add(' from pcmovendpend                                                                                   ');
     SQL.Add(' join pcendereco      on pcendereco.codendereco = pcmovendpend.codenderecoorig                       ');
     SQL.Add(' join bodefineondai   on bodefineondai.numtranswms = pcmovendpend.numtranswms                        ');
-    SQL.Add('     AND bodefineondai.numcar = pcmovendpend.numcar ');
+//    SQL.Add('     AND bodefineondai.numcar = pcmovendpend.numcar ');
     SQL.Add(' left join booscompendencia on booscompendencia.numos = pcmovendpend.numos            ');
     SQL.Add('     and booscompendencia.dataliberacao is null                                       ');
 
@@ -1216,7 +1216,7 @@ begin
     SQL.Add('    from pcmovendpend                                                                                                                       ');
     SQL.Add('    join pcendereco      on pcendereco.codendereco = pcmovendpend.codendereco                                                               ');
     SQL.Add('    left join bodefineondai   on bodefineondai.numtranswms = pcmovendpend.numtranswms                                                       ');
-    SQL.Add('               AND bodefineondai.numcar = pcmovendpend.numcar ');
+//    SQL.Add('               AND bodefineondai.numcar = pcmovendpend.numcar ');
     SQL.Add('    left join carregamentos on carregamentos.numcar = pcmovendpend.numcar                                                                   ');
     SQL.Add('    left join booscompendencia on booscompendencia.numos = pcmovendpend.numos                                                               ');
     SQL.Add('        and booscompendencia.dataliberacao is null                                                                                          ');
@@ -1506,7 +1506,7 @@ begin
     SQL.Add('     , bodefineondai.numordem                                                                                        ');
     SQL.Add('   from pcmovendpend                                                                                                 ');
     SQL.Add('   join bodefineondai  on bodefineondai.numtranswms = pcmovendpend.numtranswms                                       ');
-    SQL.Add('                       AND bodefineondai.numcar = pcmovendpend.numcar ');
+//    SQL.Add('                       AND bodefineondai.numcar = pcmovendpend.numcar ');
     SQL.Add('                       and bodefineondai.data >= pcmovendpend.data                                                   ');
     SQL.Add('   where pcmovendpend.data >= trunc(sysdate - 10)                                                                    ');
     SQL.Add('   and pcmovendpend.codfilial = :CODFILIAL                                                                           ');
@@ -1728,7 +1728,7 @@ begin
     SQL.Add(' from pcmovendpend                                                                      ');
     SQL.Add(' join pcendereco      on pcendereco.codendereco = pcmovendpend.codenderecoorig          ');
     SQL.Add(' left join bodefineondai   on bodefineondai.numtranswms = pcmovendpend.numtranswms      ');
-    SQL.Add('                         AND bodefineondai.numcar = pcmovendpend.numcar ');
+//    SQL.Add('                         AND bodefineondai.numcar = pcmovendpend.numcar ');
     SQL.Add(' join pcest on pcest.codfilial = pcmovendpend.codfilial                                 ');
     SQL.Add('     and pcest.codprod = pcmovendpend.codprod                                           ');
     SQL.Add(' left join booscompendencia on booscompendencia.numos = pcmovendpend.numos            ');
